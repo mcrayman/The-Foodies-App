e77f8fc8d8d2a12eb3eab784c6404c170cc3257e
Matt McMeans <mrmcmans@memphis.edu>
2024-02-02 21:15:31 -0600

    Update main header and nav links

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -4,28 +4,29 @@ import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
-    return (
-        <>
-            <MainHeaderBackground />
-            <header className={classes.header}>
-                <Link className={classes.logo} href="/">
-                    <Image src={logoImg} alt="A plate with food on it" priority/>
-                    NextLevel Food
-                </Link>
-            
-                <nav className={classes.nav}>
-                    <ul>
-                        <li>
-                            <Link href="/meals">Browse Meals</Link>
-                        </li>
-                        <li>
-                            <Link href="/community">Foodies Community</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    );
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
