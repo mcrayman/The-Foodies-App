0df617296f9c720d36ff3351db2d5f9c9f21a350
Matt McMeans <mrmcmans@memphis.edu>
2024-02-03 22:15:52 -0600

    Add database

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,22 +1,27 @@
-import Link from 'next/link';
-import MealsGrid from '@/components/meals/meals-grid';
-import classes from './page.module.css';
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import { getMeals } from '@/lib/meals'; 
 
-export default function MealsPage() {
-    return <>
-        <header className={classes.header}>
-            <h1>
-                Delicious meals, created <span className={classes.highlight}>by you</span>
-            </h1>
-            <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
-            <p className={classes.cta}>
-                <Link href="/meals/share">
-                    Share Your Favorite Recipe
-                </Link>
-            </p>
-        </header>
-        <main className={classes.main}>
-            <MealsGrid meals={[]}/>
-        </main>
+export default async function MealsPage() {
+  const meals = await getMeals();
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
     </>
+  );
 }
