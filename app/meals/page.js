6afb271b9a01d049e9f8ae11415c92a967f9716d
Matt McMeans <mrmcmans@memphis.edu>
2024-02-03 22:30:03 -0600

    Add loading fallback

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import classes from "./page.module.css";
-import { getMeals } from '@/lib/meals'; 
+import { getMeals } from "@/lib/meals";
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +26,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
