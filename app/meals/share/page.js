4bc4ff439a1cd2000ba16a90f46408c8e1746b85
Matt McMeans <mrmcmans@memphis.edu>
2024-02-04 11:07:56 -0600

    Add share meals form data action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,8 @@
-import classes from './page.module.css';
+'use client';
+
+import classes from "./page.module.css";
+import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
   return (
@@ -10,7 +14,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,7 +42,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your image" name="image"/>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
